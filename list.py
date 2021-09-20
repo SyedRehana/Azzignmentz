837bb9152c49a1ec9640319959f094c6210863aa
SyedRehana <91021740+SyedRehana@users.noreply.github.com>
2021-09-21 00:29:09 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,27 @@
+#Lists
+x=1
+print(x)
+l1=[1,2,3,4,5,6]
+print(l1)
+print(len(l1))
+print(l1[3])
+print(l1[5])
+l1.remove(4)
+print(l1)
+l1.append(15)
+print(l1)
+l1[2]=12
+print(l1)
+ 
+#Strings
+a='Captain'
+b=' Gowtham'
+print(a + b)
+(b)= ' Menon'
+print(a+(b)) 
+h ="Hello,world"
+print(h[1])
+print(h[7])
+print(len(h))
+ 
+ 
